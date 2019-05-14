12a94d6f6bcacde74bfb72c72b14cc857c8d4328
Abdoul-Karim SOUMAHORO <soumabkar@gmail.com>
2019-05-14 11:23:29 +0200

    require('os')

--- app.js
+++ app.js
@@ -2,17 +2,56 @@ function sayHello(name) {
     console.log('Hello '+name);
 }
 sayHello('Abdoul-Karim');
-const logger = require('./logger');
 
-logger('Kotébaor');
+//path
  const path = require('path');
  var pathObj = path.parse(__filename);
  console.log(pathObj);
 
+ //operating system
  const os = require('os');
  var totalMemory = os.totalmem();
  var freeMemory = os.freemem();
  function sayInfoOs(){
      console.log('Hello '+totalMemory);
-     console.log('Hello '+totalMemory);
+     console.log('Hello '+freeMemory);
  }
+sayInfoOs();
+// directory
+ const fs = require('fs');
+ //const files = fs.readdirSync('./');
+ //console.log(files);
+
+fs.readdir('./', function(err, files){
+    if(err) console.log('Error', err);
+    else console.log('Result', files);
+});
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+// Register a listener
+logger.on('messageLogged', (arg) =>{
+    console.log('Listener called ...', arg);
+});
+logger.log('message');
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3,4,5,6,7,8,9]));
+        res.end();
+    }
+    }
+
+);
+/*
+server.on('connection', (socket)=>{
+    console.log('New connection ...');
+});*/
+server.listen(3000);
+console.log('Listening on port 3000 ...') ;
--- logger.js
+++ logger.js
@@ -1,10 +1,16 @@
+const EventEmitter = require('events');
 
     var url = 'http://mylogger.io/log';
 
-    function log(message) {
-        //send HTTp request
-        console.log('Bonjour MR :' + message);
+    class Logger extends EventEmitter {
+        log(message){
+            //send HTTp request
+            console.log('Bonjour MR :' + message);
+            //  Raise an event
+            this.emit('messageLogged',{id: 1, url: 'http://'});
+        }
     }
-    module.exports = log;
+
+    module.exports = Logger;
 
 
